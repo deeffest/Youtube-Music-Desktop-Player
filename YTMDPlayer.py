15a6fb244452709bb9363abab89607c654e50ac4
deeffest <deeffestt@gmail.com>
2025-03-27 12:27:53 +0300

    v1.17.4 is Here! (27.03.2025)
    - Fixed track/playlist downloader.

--- YTMDPlayer.py
+++ YTMDPlayer.py
@@ -11,7 +11,7 @@ from core.main_window import MainWindow
 NAME = "Youtube-Music-Desktop-Player"
 AUTHOR = "deeffest"
 WEBSITE = "deeffest.pythonanywhere.com"
-VERSION = "v1.17.3"
+VERSION = "v1.17.4"
 UNIQUE_KEY = f"{AUTHOR}.{NAME}"
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
